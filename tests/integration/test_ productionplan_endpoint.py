11ca775f4cfa456cf629cd9cf7601fd84c0ec0b2
Dries Desmet <dries@urga.be>
2023-03-20 17:13:51 +0100

    Improve test by basing it on provided example data

--- tests/integration/test_ productionplan_endpoint.py
+++ tests/integration/test_ productionplan_endpoint.py
@@ -1,6 +1,8 @@
 from rest_framework import status
 
+# positive tests (happy paths)
+##############################
 
-def test_create_productionplan_status_code(client):
-    response = client.post('/productionplan')
+def test_create_productionplan_status_code(client, example_payload1):
+    response = client.post('/productionplan', example_payload1, format='json')
     assert response.status_code == status.HTTP_201_CREATED
